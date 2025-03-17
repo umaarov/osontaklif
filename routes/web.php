4fedea75958a4b11253fb302ed80d52fefd5ff2c
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-17 19:19:17 +0500

    Update routes to use PageController for home, mock, and requirements pages; Starter app

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/mock', [PageController::class, 'mock'])->name('mock');
+Route::get('/requirements', [PageController::class, 'requirements'])->name('requirements');
