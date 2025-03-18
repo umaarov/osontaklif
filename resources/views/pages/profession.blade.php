09bd6ecae0f7045bd0518ae6f7f5f6b07bb55d9a
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-18 15:08:45 +0500

    Update ad space label in profession view and remove back link in question view

--- resources/views/pages/profession.blade.php
+++ resources/views/pages/profession.blade.php
@@ -50,7 +50,7 @@
         <!-- Ad Space -->
         <div
             style="flex: 1; min-width: 250px; background-color: #f9f9f9; padding: 20px; text-align: center; border-radius: 5px;">
-            <p style="color: #888;">Future Ad Space</p>
+            <p style="color: #888;">Ad Space</p>
         </div>
     </div>
 @endsection
