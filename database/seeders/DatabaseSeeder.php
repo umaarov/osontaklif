629a5f4658a7b10cf9ecd1baee9e4d885a82868c
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-04-15 14:48:24 +0500

    integrate android questions, delete android questions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,51 +2,18 @@
 
 namespace Database\Seeders;
 
-use App\Models\Interview;
-use App\Models\Profession;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    final function run(): void
+    final public function run(): void
     {
-        $professions = [
-            'java' => 'Java',
-            'python' => 'Python',
-            'php' => 'PHP',
-            'android' => 'Android',
-            'ios' => 'iOS',
-            'flutter' => 'Flutter',
-            'node' => 'Node.js',
-            'react' => 'React',
-            'vue' => 'Vue',
-            'html-css' => 'HTML & CSS',
-            'devops' => 'DevOps',
-            'csharp' => 'CSharp',
-            'cpp' => 'C++',
-            'js' => 'JavaScript',
-            'golang' => 'Golang',
-            'sql' => 'SQL',
-            'qa' => 'Quality Assurance',
-            'ux-ui' => 'UX & UI',
-            'project' => 'Project Manager',
-            'seo' => 'SEO',
-        ];
+        $this->call([
+            ProfessionSeeder::class,
+            InterviewSeeder::class,
+        ]);
 
-        $grades = ['Junior', 'Middle', 'Senior'];
-
-        foreach ($professions as $key => $name) {
-            $profession = Profession::firstOrCreate(['name' => $name]);
-            foreach ($grades as $grade) {
-                Interview::create([
-                    'title' => "$name Interview",
-                    'link' => 'https://kun.uz',
-                    'profession_id' => $profession->id,
-                    'grade' => $grade,
-                ]);
-            }
-        }
-        $this->call(AndroidQuestionsSeeder::class);
         $this->command->info('All specified seeders have been run.');
     }
+
 }
